3da6d95f87fa4283262855d8f38273ce4c497208
silasmas <silasjmas@gmail.com>
2025-03-24 02:37:31 +0100

    Update AdminPanelProvider.php

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('dashboard')
             ->login()
+
             ->colors([
                 'primary' => Color::Amber,
             ])
