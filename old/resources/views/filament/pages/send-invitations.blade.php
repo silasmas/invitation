6b968b8d7e08e4736fce1bda1f32e03926c21f71
silasmas <silasjmas@gmail.com>
2025-03-25 02:11:29 +0100

    dd

--- old/resources/views/filament/pages/send-invitations.blade.php
+++
@@ -1,6 +0,0 @@
-<x-filament-panels::page>
-    <form wire:submit.prevent="submit">
-        {{ $this->form }}
-        <x-filament::button type="submit" class="mt-4">Envoyer les invitations</x-filament::button>
-    </form>
-</x-filament-panels::page>
