c214857ed0dd5efa86b9b56c9d2d4158fd237994
silasmas <silasjmas@gmail.com>
2025-05-28 19:58:15 +0100

    Update show.blade.php

--- resources/views/invitation/show.blade.php
+++ resources/views/invitation/show.blade.php
@@ -140,7 +140,7 @@
                                             </div>
                                         </div>
                                         <div class="wedding-address">
-                                            <h3 class="uppercase my-3" style="border: #777 solid !important">Cérémonie du mariage {{ $invitation->ceremonies->nom }}
+                                            <h3 class="uppercase my-3" style="border: 1px solid #777; border-radius: 50px; padding: 10px;">Cérémonie du mariage {{ $invitation->ceremonies->nom }}
                                             </h3>
                                             <h5> {!! $invitation->ceremonies->adresse !!}</h5>
                                         </div>
@@ -182,7 +182,7 @@
                                             </div>
                                         </div>
                                         <div class="wedding-address">
-                                            <h3 class="uppercase my-3">Cérémonie du mariage {{ $invitation->ceremonies->nom }}
+                                            <h3 class="uppercase my-3" style="border: 1px solid #777; border-radius: 50px; padding: 10px;">Cérémonie du mariage {{ $invitation->ceremonies->nom }}
                                             </h3>
                                             <h5> {!! $invitation->ceremonies->adresse !!}</h5>
                                         </div>
